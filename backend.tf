349ecac48557be914cffc9e733e51b1cdc4de815
MinThuKha <87811913+mtk2303@users.noreply.github.com>
2025-08-01 13:03:27 +0630

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "hoodbu"
+    organization = "mintkha"
     workspaces {
       name = "ace-iac-day-zero"
     }
